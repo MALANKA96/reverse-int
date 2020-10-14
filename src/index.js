223e74744dc7eb0ec01bc941d5e85596cb983afe
malanka96 <voksa.kolya@gmail.com>
2020-10-14 19:46:11 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let a = 0;
+    if (n < 0) { a = Math.abs(n); 
+  return  Number(a.toString().split("").reverse().reduce((x,y) => x+y));}
+  return  Number(n.toString().split("").reverse().reduce((x,y) => x+y));
 }
